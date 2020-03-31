6be51bafbefb9c8a8c67cd0ceae7bf21e724a489
potonix <690532597@qq.com>
2020-03-31 11:12:47 +0000

    fix bug

--- scripts/cmds-in-ctrlbox/create-index-lifecycle.sh
+++ scripts/cmds-in-ctrlbox/create-index-lifecycle.sh
@@ -15,12 +15,12 @@ echo
 (
   cd $index_lifecyle_home
   for n in `ls`; do 
-    response=`curl -s -o /dev/null $host_endpoint/_ilm/police/$n -w "%{http_code}"`
+    response=`curl -s -o /dev/null $host_endpoint/_ilm/policy/$n -w "%{http_code}"`
     if [ "$response" != "200" ]; then
       echo -n "Creating index_lifecyle: $n ... "
       curl -X PUT -s -w "%{http_code}" \
         -H "Content-Type: application/json" \
-        $host_endpoint/_ilm/police/$n -d@$n
+        $host_endpoint/_ilm/policy/$n -d@$n
       echo
     fi
   done
