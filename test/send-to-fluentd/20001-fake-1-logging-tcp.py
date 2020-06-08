1daad8b694c208da95e90773c7d57782f1d55cde
potonix <690532597@qq.com>
2020-06-08 15:47:40 +0800

    optimize fluentd conf and test scripts

--- test/send-to-fluentd/20001-fake-1-logging-tcp.py
+++ test/send-to-fluentd/20001-fake-1-logging-tcp.py
@@ -1,7 +1,7 @@
 import testlib
 import time
 
-testlib.send_logs.udp_connect('fluentd', 20001)
+testlib.send_logs.tcp_connect('localhost', 20001)
 
 # ts = testlib.timeutils.str2ts('2020-01-02T10:40:00.000Z')
 ts = int(time.time())
@@ -11,9 +11,9 @@ ts = int(time.time())
 # s2 = testlib.timeutils.ts2str(t1)
 
 log = testlib.mock_data.mock_logging_data_20001(ts, 1)
-testlib.send_logs.send_udp(log)
+testlib.send_logs.send_tcp(log)
 print(log)
-testlib.send_logs.udp_socket.close()
+testlib.send_logs.tcp_socket.close()
 
 '''
 {"username": "zongzw", "status": "200", "client-ip": "103.120.100.39", "timestamp": "2020-01-02T10:08:00.000Z", "host": "bigip-vs-server", "cookie": "", "latency": 4, "sender": "zongzw 17", "uri": "/", "user-agent": "Opera/9.80 (X11; U; Linux i686; en-US; rv:1.9.2.3) Presto/2.2.15 Version/10.10", "server-ip": "10.250.11.24", "content-type": "application/json", "method":"GET"}
